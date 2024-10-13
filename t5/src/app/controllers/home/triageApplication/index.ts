70f517b8d5f717a1f6c15c0e5e7631e38d6f60a7
awatson-sp <awatson@sponsorpulse.com>
2024-10-13 12:47:33 -0700

    Prisma setup

--- t5/src/app/controllers/home/triageApplication/index.ts
+++ t5/src/app/controllers/home/triageApplication/index.ts
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
+import Prisma from "../../../../../prisma/prisma";
 //import the database object used. The singleton
 
 export default class TriageController {
   public static async getTriageApplication() {
-    const response = await fetch("/api/home/triageApplication");
-    return response.json();
   }
 };
   
