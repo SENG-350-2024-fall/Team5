449b737a5b79ec1eff94da6b5fb19c15f251a2b6
awatson-sp <awatson@sponsorpulse.com>
2024-11-03 08:57:38 -0800

    API routes deleted as not needed since no db implemented

--- t5/src/app/api/home/route.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-  return NextResponse.json({ message: "GET request received", status: 200 });
-}
