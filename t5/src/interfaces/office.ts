b02b0bc971a911fc628fb7b86463ab0d773e5979
Juhlo <Juhlo@uvic.ca>
2024-11-13 20:34:02 -0800

    added an option for office_type to be none, in case there is an error and no office is found

--- t5/src/interfaces/office.ts
+++ t5/src/interfaces/office.ts
@@ -5,5 +5,5 @@ export interface OFFICE {
   phone_number: String | null;
   ave_wait_time: Date | null;
   name: String;
-  office_type: "Call center" | "Pharmacy" | "Clinic" | "Emergency room";
+  office_type: "Call center" | "Pharmacy" | "Clinic" | "Emergency room" | "None";
 }
